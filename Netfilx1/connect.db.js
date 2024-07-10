27a8532f09a409e6aed93c3a3996c38f82fb9183
ssmarika <smarikashrestha02@gmail.com>
2024-07-10 16:43:11 +0545

    Netflix

--- Netfilx1/connect.db.js
+++ Netfilx1/connect.db.js
@@ -3,11 +3,12 @@ import mongoose from "mongoose";
 const dbUserName = "smarikashrestha02";
 const dbPassword = encodeURIComponent("smarika123");
 const dbName = "kec-b4-netfilix";
-const dbHost = "smarika123@cluster0.yv6arsk.mongodb.net";
+const dbHost = "cluster0.yv6arsk.mongodb.net";
+
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://smarikashrestha02:smarika123@cluster0.yv6arsk.mongodb.net/kec-b4-netflix?retryWrites=true&w=majority&appName=Cluster0"
+      `mongodb+srv://${dbUserName}:${dbPassword}@${dbHost}/${dbName}?retryWrites=true&w=majority&appName=Cluster0`
     );
     console.log("DB connection established");
   } catch (error) {
--- Netfilx1/movie.model.js
+++ Netfilx1/movie.model.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 // set scheme(rule)
-
+// defining the table
 const movieSchema = new mongoose.Schema({
   name: String,
   rating: Number,
