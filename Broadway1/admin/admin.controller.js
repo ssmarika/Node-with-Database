673a60335724f7b148ece766f6b6c049b06f94f6
ssmarika <smarikashrestha02@gmail.com>
2024-07-17 14:24:52 +0545

    Commit

--- Broadway1/admin/admin.controller.js
+++ Broadway1/admin/admin.controller.js
@@ -1,33 +1,41 @@
 import express from "express";
-import { validateData } from "./admin.validation.js";
+import { adminValidationSchema } from "./admin.validation.js";
 import Admin from "./admin.model.js";
 import bcrypt from "bcrypt";
+import validateReqBody from "../middlewares/validation.middleware.js";
 
 const router = express.Router();
 
 // ? add router
-router.post("/add", validateData, async (req, res) => {
-  // extract new admin from req.body
-  const newAdmin = req.body;
+router.post(
+  "/add",
+  validateReqBody(adminValidationSchema),
 
-  //find admin using provided email
-  const admin = await Admin.findOne({ email: newAdmin.email });
+  async (req, res) => {
+    // extract new admin from req.body
+    const newAdmin = req.body;
 
-  //if admin exist, throw error
+    //find admin using provided email
+    const admin = await Admin.findOne({ email: newAdmin.email });
 
-  if (admin) {
-    return res.status(409).send({ message: "Admin already exist" });
+    //if admin exist, throw error
+
+    if (admin) {
+      return res.status(409).send({ message: "Admin already exist" });
+    }
+
+    // generate hashed password
+    // we hash the password for the purpose of safety.
+    // when we use unhashed password, password are visible in database which is a security threat
+    const plainPassword = newAdmin.password;
+    const saltRound = 10; //increase randomness
+    const hashedPassword = await bcrypt.hash(plainPassword, saltRound);
+
+    newAdmin.password = hashedPassword;
+
+    await Admin.create(newAdmin);
+
+    return res.status(201).send({ message: "Adding" });
   }
-
-  //generate hashed password
-  const plainPassword = newAdmin.password;
-  const saltRound = 10; //increase randomness
-  const hashedPassword = await bcrypt.hash(plainPassword, saltRound);
-
-  newAdmin.password = hashedPassword;
-
-  await Admin.create(newAdmin);
-
-  return res.status(201).send({ message: "Adding" });
-});
+);
 export default router;
--- Broadway1/middlewares/validation.middleware.js
+++ Broadway1/middlewares/validation.middleware.js
@@ -2,7 +2,7 @@ const validateReqBody = (validationSchema) => {
   return async (req, res, next) => {
     const data = req.body;
     try {
-      const validatedData = await dataSchema.validate(data);
+      const validatedData = await validationSchema.validate(data);
       req.body = validatedData;
     } catch (error) {
       return res.status(404).send({ message: error.message });
